206ea4506da82da86c3401f4b969fe3feb652141
azhao915 <50418896+azhao915@users.noreply.github.com>
2025-11-09 17:50:56 -0500

    Apply .gitignore

--- build/lib/seegview/config.py
+++
@@ -1,5 +0,0 @@
-# A file to store all the paths used for the demo 
-# Replace these values following the instructions in the README.md
-
-bids_root = r"D:/DABI/StimulationDataset"
-freesurfer_root = r"D:\DABI\StimulationDataset\derivatives\freesurf"
