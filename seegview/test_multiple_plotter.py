f37d924ccbf65b02f1ec63a2a6c69b6ec207bfff
azhao915 <50418896+azhao915@users.noreply.github.com>
2025-11-28 14:59:27 -0500

    Tried Multiple Plotting, didn't work, will try again later

---
+++ seegview/test_multiple_plotter.py
@@ -0,0 +1,28 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+import os
+
+from seegview.load_reference_dataset import load
+
+import numpy as np
+
+from seegview.Widgets.MultipleTimeWidget import MultipleTimeWidget
+
+from seegview.Managers.TimeManager import TimeManager
+from seegview.Managers.KeypressManager import KeybindingManager
+
+app = QApplication(sys.argv)
+
+raw = load(0)
+
+# The Multiple Browser
+browser = MultipleTimeWidget(
+    raw, 
+    curr_channel = 0, 
+    curr_time = 0.0, 
+    window_duration = 10.0,
+    num_traces = 10
+)
+
+keybinding_manager = KeybindingManager(browser)
+browser.show()
